4f19b4e4e7d46305c470871173920939e692b2b5
TanyaRan <tatyanaran@gmail.com>
2020-12-28 20:19:43 +0200

    create CharactersCollection class to sort string data type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, -12, 5, 8, 0, -1]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('something');
+const sorter1 = new Sorter(charactersCollection);
+sorter1.sort();
+console.log(charactersCollection.data);
