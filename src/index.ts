c480136e3cabd1d2e1acc9da930f0d3e80ddb187
TanyaRan <tatyanaran@gmail.com>
2020-12-28 19:42:51 +0200

    separate swap and compare logic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello!');
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+
+const numbersCollection = new NumbersCollection([10, -12, 5, 8, 0, -1]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
