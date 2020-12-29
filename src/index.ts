c805432d147b5781e592324adcd7d7e7e1694d2e
TanyaRan <tatyanaran@gmail.com>
2020-12-29 23:17:28 +0200

    basic LinkedList sort

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, -12, 5, 8, 0, -1]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection('something');
 const sorter1 = new Sorter(charactersCollection);
 sorter1.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(100);
+linkedList.add(-200);
+linkedList.add(50);
+linkedList.add(-10);
+const sorter2 = new Sorter(linkedList);
+sorter2.sort();
+linkedList.print();
