53c00582d1ba23208723fa621dfc7802867b28c7
TanyaRan <tatyanaran@gmail.com>
2020-12-30 00:11:12 +0200

    use abstract Sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, -12, 5, 8, 0, -1]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('something');
-const sorter1 = new Sorter(charactersCollection);
-sorter1.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -19,6 +16,5 @@ linkedList.add(100);
 linkedList.add(-200);
 linkedList.add(50);
 linkedList.add(-10);
-const sorter2 = new Sorter(linkedList);
-sorter2.sort();
+linkedList.sort();
 linkedList.print();
